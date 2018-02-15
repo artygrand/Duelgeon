ce8e0c26e68dca23ac303ef811bb67509a11a174
Arthur Grand <artygrand@gmail.com>
2018-02-15 17:55:23 +0700

    Updated base scene

--- Scenes/BaseScene.py
+++ Scenes/BaseScene.py
@@ -1,8 +1,26 @@
 #!/usr/bin/env python3
 
+from Utils import win
+
 
 class BaseScene:
+    paused = False
+
     def esc_handler(self):
         # TODO show mini-menu
-        print('Pause menu')
+        if self.paused:
+            print('Pause menu close')
+            win.hide_cursor()
+            self.resume()
+        else:
+            self.pause()
+            win.show_cursor()
+            print('Pause menu open')
 
+        self.paused = not self.paused
+
+    def pause(self):
+        pass
+
+    def resume(self):
+        pass
