9193fa0cb03078ef209c5d01568d75096b8a326c
Arthur Grand <artygrand@gmail.com>
2018-02-20 21:29:54 +0700

    Added Bullet triangle shape generator

--- Utils/geom.py
+++ Utils/geom.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 from panda3d.core import LVector3
+from panda3d.bullet import BulletTriangleMeshShape, BulletTriangleMesh
 
 
 def normalized(*args):
@@ -8,3 +9,15 @@ def normalized(*args):
     vec.normalize()
 
     return vec
+
+
+def bullet_shape_from(source, dynamic=False):
+    mesh = BulletTriangleMesh()
+
+    for geom_node in source.findAllMatches('**/+GeomNode'):
+        gn = geom_node.node()
+        ts = gn.getTransform()
+        for geom in gn.getGeoms():
+            mesh.addGeom(geom, True, ts)
+
+    return BulletTriangleMeshShape(mesh, dynamic=dynamic)
